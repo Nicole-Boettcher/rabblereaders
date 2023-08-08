341402c9e16529b4bf47716e8fb04dce5e269e4e
Nicole-Boettcher <71027559+Nicole-Boettcher@users.noreply.github.com>
2023-08-08 16:17:36 -0400

    User data persists, bug with removing local storage

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -4,47 +4,28 @@ import { useEffect, useState } from "react";
 
 function HomeScreen(props) {
   //when this page first loads, it should take the user data and fetch the profile from the db so it is saved
-  let isPageRefresh = false;
-  localStorage.setItem('userData', JSON.stringify(props.userData));
   const [userData, setUserData] = useState(props.userData); // Use storedUserData as initial state
 
   useEffect(() => {
-    
-  
-    const handleBeforeUnload = () => {
-      isPageRefresh = true;
-    };
-  
-    window.addEventListener('beforeunload', handleBeforeUnload);
-  
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  
-  });
+    console.log("Getting")
+    const data = window.localStorage.getItem('USER_DATA_HOME');
+    if (data !== null ) setUserData(JSON.parse(data));
+  }, []);
 
   useEffect(() => {
-    console.log("invoked")
-      const storedUserData = JSON.parse(localStorage.getItem('userData'));
-
-      if (storedUserData) {
-        console.log("setting data")
-        console.log(storedUserData)
-        setUserData(storedUserData);
-      }
-    
-  }, [isPageRefresh])
+    console.log("Setting")
+    window.localStorage.setItem('USER_DATA_HOME', JSON.stringify(userData));
+  }, [userData])
   // TODO: MAKE ABOVE RELOAD ONLY WHEN PAGE IS AUTOMATICALLY REFRESHED
 
-  if (!userData) {
-    return <div>Loading...</div>;
-  }
+  // if (!userData) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div>
       <h1>Home Screen</h1>
-      <p>Username: {userData.Username}</p>
+      {userData && <p>Username: {userData.Username}</p>}
       {/* <p style={{ textAlign: 'right' }}>Password: {props.loginData.password}</p> */}
 
       <p>Current Book Clubs:</p>
